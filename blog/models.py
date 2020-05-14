d323c2cb371b58a16c6ca17dc7fe26ea2e55db44
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2020-05-14 03:07:16 +0300

    Add the model for the Blogs. Feature. This feature enables us to structure how each blog post object will look like. The change creates a BlogPost table where we can save our records. This commit also allows images to be uploaded directly to Cloudinary.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = (
+    (0, 'Draft'),
+    (1, 'Publish')
+)
+
+class BlogPost(models.Model):
+    featured_image = CloudinaryField('media')
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
